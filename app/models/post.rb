524264a2867dd575755ea3e947a053fa859b09b9
Haleem Hamid <hhamidbiz@gmail.com>
2022-02-23 14:39:58 -0800

    commit tree 606fb87b0a1c884d484bcb792fe259204e8b747e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+  validate :correct_title
+
+  def correct_title
+     if :title.downcase == 'true facts'
+        errors.add(:title, "can't be Ture Facts")
+  end
+end
 end
